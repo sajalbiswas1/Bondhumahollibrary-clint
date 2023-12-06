e42c9113beb97abad8a73e6a753560a01d39e9f7
Programing Hero <sajalb1899@gmail.com>
2023-12-07 00:01:30 +0600

    from section added

--- src/Page/HomePage/Section/Form.jsx
+++ src/Page/HomePage/Section/Form.jsx
@@ -1,8 +1,16 @@
-// import { FaArrowAltCircleLeft } from "react-icons/fa";
+import { FaArrowAltCircleLeft } from "react-icons/fa";
 const Form = () => {
     return (
         <div className="flex items-center gap-5 m-10">
-            
+            <a href="https://docs.google.com/forms/d/1qw5tMe-gBvV5HCg32KgoUn92xmj3jsHaTQxLqnpXbtA/edit" className="text-center md:p-10 pt-10 h-40 w-40 bg-slate-200">
+                <p>Form Here</p>
+                <p>Click Now</p>
+
+            </a>
+            <div className="flex items-center gap-2 bg-slate-50">
+                <FaArrowAltCircleLeft className="text-xl ml-2" />
+                <p className=" py-1 px-3 ">ফরম পূরণ করতে এখানে ক্লিক</p>
+            </div>
         </div>
     );
 };
