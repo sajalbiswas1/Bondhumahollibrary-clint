f10435ed009df38f863754028c33ab7814a90e41
Programing Hero <sajalb1899@gmail.com>
2023-12-10 20:21:17 +0600

    from input valu collect

--- src/Page/HomePage/Section/ManualForm.jsx
+++ src/Page/HomePage/Section/ManualForm.jsx
@@ -5,14 +5,19 @@ const ManualForm = () => {
         const form = e.target;
         const name = form.name.value;
         const fatherName = form.fatherName.value;
-        // const name = form.name.value;
-        // const name = form.name.value;
-        // const name = form.name.value;
-        // const name = form.name.value;
-        // const name = form.name.value;
-        // const name = form.name.value;
-        // const name = form.name.value;
-        // const name = form.name.value;
+        const motherName = form.motherName.value;
+        const permanentAddress = form.name.value;
+        const presentAddress = form.presentAddress.value;
+        const nationality = form.nationality.value;
+        const religion = form.religion.value;
+        const userAge = form.userAge.value;
+        const nidNumber = form.nidNumber.value;
+        const relationShip = form.relationShip.value;
+        const other = form.other.value;
+        const email = form.email.value;
+        const phoneNumber = form.phoneNumber.value;
+        const obj = {name,fatherName,motherName,permanentAddress,presentAddress,nationality,religion,userAge,nidNumber,relationShip,other,email,phoneNumber}
+        console.log(obj);
     }
     return (
         <div>
@@ -39,13 +44,13 @@ const ManualForm = () => {
                         <label className="label">
                             <span className="label-text font-bold text-lg">মাতার নাম</span>
                         </label>
-                        <input type="text" placeholder="মাতার নাম" className="input input-bordered" required />
+                        <input type="text" name="motherName" placeholder="মাতার নাম" className="input input-bordered" required />
                     </div>
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text font-bold text-lg">স্থায়ী ঠিকানা :</span>
                         </label>
-                        <input type="text" placeholder="গ্রাম,ডাকঘর,উপজেলা,জেলা" className="input input-bordered" required />
+                        <input type="text" name="permanentAddress"  placeholder="গ্রাম,ডাকঘর,উপজেলা,জেলা" className="input input-bordered" required />
                     </div>
                 </div>
                 <div className="grid md:grid-cols-2 gap-5">
@@ -53,13 +58,13 @@ const ManualForm = () => {
                         <label className="label">
                             <span className="label-text font-bold text-lg">বর্তমান ঠিকানা :</span>
                         </label>
-                        <input type="text" placeholder="বর্তমান ঠিকানা" className="input input-bordered" required />
+                        <input type="text" name="presentAddress" placeholder="বর্তমান ঠিকানা" className="input input-bordered" required />
                     </div>
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text font-bold text-lg">জাতীয়তা :</span>
                         </label>
-                        <input type="text" placeholder="জাতীয়তা " className="input input-bordered" required />
+                        <input type="text" name="nationality" placeholder="জাতীয়তা " className="input input-bordered" required />
                     </div>
                 </div>
                 <div className="grid md:grid-cols-2 gap-5">
@@ -67,7 +72,7 @@ const ManualForm = () => {
                         <label className="label">
                             <span className="label-text font-bold text-lg">ধর্ম :</span>
                         </label>
-                        <select className="input input-bordered" name="ধর্ম" id="">
+                        <select className="input input-bordered" name="religion" id="">
                             <option value="বিবাহিত">হিন্দু </option>
                             <option value="অবিবাহিত">ইসলাম</option>
                             <option value="অবিবাহিত">খ্রিষ্টান</option>
@@ -79,7 +84,7 @@ const ManualForm = () => {
                         <label className="label">
                             <span className="label-text font-bold text-lg">বয়স :</span>
                         </label>
-                        <input type="text" placeholder="বয়স " className="input input-bordered" required />
+                        <input type="text" name="userAge" placeholder="বয়স " className="input input-bordered" required />
                     </div>
                 </div>
                 <div className="grid md:grid-cols-2 gap-5">
@@ -87,14 +92,14 @@ const ManualForm = () => {
                         <label className="label">
                             <span className="label-text font-bold text-lg">জাতীয়পরিচয় পত্র নম্বর : </span>
                         </label>
-                        <input type="text" placeholder="জাতীয়পরিচয় পত্র নম্বর " className="input input-bordered" required />
+                        <input type="text" name="nidNumber" placeholder="জাতীয়পরিচয় পত্র নম্বর " className="input input-bordered" required />
                     </div>
                     <div className="form-control">
 
                         <label className="label">
                             <span className="label-text font-bold text-lg">বৈবাহিক অবস্থা :</span>
                         </label>
-                        <select className="input input-bordered" name="বৈবাহিক অবস্থা" id="">
+                        <select className="input input-bordered" name="relationShip" id="">
                             <option value="বিবাহিত">বিবাহিত</option>
                             <option value="অবিবাহিত">অবিবাহিত</option>
                         </select>
@@ -106,20 +111,20 @@ const ManualForm = () => {
                         <label className="label">
                             <span className="label-text font-bold text-lg">অন্যান্য :</span>
                         </label>
-                        <input type="text" placeholder="অন্যান্য" className="input input-bordered" />
+                        <input type="text" name="other" placeholder="অন্যান্য" className="input input-bordered" />
                     </div>
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text font-bold text-lg">ইমেইল :</span>
                         </label>
-                        <input type="email" placeholder="rohim@gmail.com" className="input input-bordered" required />
+                        <input type="email" name="email" placeholder="rohim@gmail.com" className="input input-bordered" required />
                     </div>
                 </div>
                 <div className="form-control">
                     <label className="label">
                         <span className="label-text font-bold text-lg">ফোন নাম্বার :</span>
                     </label>
-                    <input type="text" placeholder="018xxxxxxx" className="input input-bordered" required />
+                    <input type="text" name="phoneNumber" placeholder="018xxxxxxx" className="input input-bordered" required />
                 </div>
                 <div className="form-control mt-6">
                     <button className="btn btn-primary">Registration</button>
